cabd96a64a6783eff50be25272edd72b88c33df8
David Ringgenberg <david.ringgenberg@ringgi.net>
2022-01-08 17:53:14 +0100

    feat: add record Button and recordAudio Funktion

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        print("record Button was pressed")
+    }
 }
 
